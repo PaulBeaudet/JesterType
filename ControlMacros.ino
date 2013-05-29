6a90f71f6105c35f6a21ad44ee36a980eb9fc446
Paul Beaudet <inof8or@gmail.com>
2013-05-29 11:00:35 -0400

    removed buggy switch case flow control

--- ControlMacros.ino
+++ ControlMacros.ino
@@ -1,26 +1,25 @@
 //ControMacros---controling opporators that imply funtions
 //such as space, enter, delete, tab, justify and so on
-//!!have been advised that switch cases are probably a bad idea
-// because they may me interperted differently my differnt MCUs
 //-------------------------------------------YES CASE
 void yesCase()
 {
-  //regular activity 
-  switch(count[YESC])
+  if(count[YESC])
   {
-  case 0://yes
-    space();
-    break;
-  case 1://yes, yes
-    period();
-     break;
-  case 2://yes, yes, yes
-    enter();
-    break;
-  default:
-    count[YESC]=0;
-    return;
+    if(count[YESC]==1)
+    {
+      period();
+    }
+    else
+    {
+      enter();
+      count[YESC]=0;
+      return;
+    }
   }
+  else
+  {
+    space();
+  };
   count[YESC]++;
 }
 //#####################################NO
@@ -28,22 +27,33 @@ void noCase()
 {
   if(count[YESC])
   {
-    switch(count[YESC])
+    if(count[YESC]<2 && count[NOC]<1)
     {
-    case 1:
-      autoFill();//or after word//yes, no
-      //whether autofill or spell correct is used depends
-      //on the amount of letters in the buffer
-      break;
-    case 2:
-      punctuation('?');
-      break;
-    case 3:
-      punctuation('!');
-      break;
-    default:
-      count[YESC]=0;//set yes to 0 in order to define specific cases
+      autoFill();
+      count[NOC]=0;
+      return;
     }
+    else if(count[YESC]==2)
+    {//if yes is at the puctuation phase
+      if(count[NOC])
+      {//and no has been previously pressed
+        if(count[NOC]==1)
+        {//that is pressed once
+          punctuation(',');//place a comma
+        }
+        else if(count[NOC]==2)
+        {//its pressed a third time
+          punctuation('!');//place an exclaimation
+          count[YESC]=0;//reset yes an no
+          count[NOC]=0;
+          return;
+        };
+      }
+      else//no has not been pressed ye
+      {
+        punctuation('?');//place a question
+      };
+    };//no is incremented in most yes no cases
     count[NOC]++;//add to the no count
   }
   else
@@ -67,9 +77,10 @@ void printLetter(byte letterNum)
     {
       letterBuffer[count[CWORD]]=letterNum;
     }
-    count[YESC]=0;
-    count[NOC]=0;
+    
   }
+  count[YESC]=0;
+  count[NOC]=0;
   countChange(1);
 }
 void countChange(byte increment)
@@ -111,32 +122,33 @@ void period()
 void autoLineReturn()
 {//for main loop
   if(count[LINEC]>LINESIZE)
-    {//one the line size limit is reached
-      if(count[CWORD])
-      {//given there are printed letters 
-        movement(count[CWORD],LEFT);//:KeyboardFunctions
-        //move the cursor to the left of the current word prepping it
-      }//to be pushed to the next line thus keeping word whole
-      enter();//regardless press enter and add the current word count
-      count[LINEC]=count[CWORD];//to the line in case a word was in progress
-    }
+  {//one the line size limit is reached
+    if(count[CWORD])
+    {//given there are printed letters 
+      movement(count[CWORD],LEFT);//:KeyboardFunctions
+      //move the cursor to the left of the current word prepping it
+    }//to be pushed to the next line thus keeping word whole
+    enter();//regardless press enter and add the current word count
+    count[LINEC]=count[CWORD];//to the line in case a word was in progress
+  }
 }
 //------------------------------------set-up function
 void setControls()
 {//prompt for setting up the control structure
-    wait();//starts when a button is pressed//:valueAssement_buttons
-    delay(REACT);//buffer for the desired value to be read
-    pressKey('y');
-    pressKey('e');
-    pressKey('s');
-    pressKey('?');//What would you like "yes" to be?
-    assign(30, getValue(),0);//which is address 60 in EEPROM  
-    pressKey('-');
-    pressKey('n');
-    pressKey('o');
-    pressKey('?');//What would you like "no" to be?
-    assign(31, getValue(),0);//which is address 62 in EEPROM
-    backSpaces(8);//Remove the prompt
+  wait();//starts when a button is pressed//:valueAssement_buttons
+  delay(REACT);//buffer for the desired value to be read
+  pressKey('y');
+  pressKey('e');
+  pressKey('s');
+  pressKey('?');//What would you like "yes" to be?
+  assign(30, getValue(),0);//which is address 60 in EEPROM  
+  pressKey('-');
+  pressKey('n');
+  pressKey('o');
+  pressKey('?');//What would you like "no" to be?
+  assign(31, getValue(),0);//which is address 62 in EEPROM
+  backSpaces(8);//Remove the prompt
 }
+
 
 
